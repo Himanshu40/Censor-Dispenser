ad9708cdee399e78e55985a3bd01877d710b2d7a
Himanshu40 <himanshuwindows8.1@gmail.com>
2020-07-21 08:59:04 +0530

    Added functions for email three and four

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -5,3 +5,66 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+# Words and phrases from email_two
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+# Negative words from email_three
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
+# Censor a specific word or phrase from a body of text and then return the text
+def censor_email_one(email, word):
+    censor_word = ""
+    for i in range(len(word)):
+        censor_word += "X"
+    return email.replace(word, censor_word)
+
+# Censor whole list of words from a body of text and then return the text
+def censor_email_two(email, terms_list):
+    email_lst    = ['']
+    email_lst[0] = email
+    for items in terms_list:
+        if items in email_lst[0]:
+            email_lst[0] = email_lst[0].replace(items, "REDACTED")
+    return email_lst[0]
+
+# Censor whole negative words from a body of text and then return the text
+def censor_email_three(email, terms_list):
+    email_lst    = ['']
+    email_lst[0] = email
+    for items in terms_list:
+        if items in email_lst[0]:
+            objectVar = email_lst[0].find(items)
+            for items2 in terms_list:
+                if items2 in email_lst[0][objectVar + 1:]:
+                    email_lst[0] = email_lst[0][:objectVar + 1] + email_lst[0][objectVar + 1:].replace(items, "REDACTED")
+    return censor_email_two(email_lst[0], proprietary_terms)  
+
+# Censors not only all of the words from the negative_words and proprietary_terms lists, but also censor any words that come before AND after a term from those two lists
+def censor_email_four(email):
+    email_lst1 = ['']
+    email_lst2 = ['']
+    email_lst3 = []
+    email_lst1[0] = email
+    email_lst1[0] = censor_email_two(email_lst1[0], proprietary_terms)
+    email_lst1[0] = censor_email_two(email_lst1[0], negative_words)
+    email_lst2[0] = email_lst1[0].replace("\n", " lineBreak ")
+    email_lst3    = email_lst2[0].split(" ")
+    y = 0
+    while y < (len(email_lst3) - 1):
+        if (email_lst3[y] == "REDACTED") or (email_lst3[y] == "REDACTEDe") or (email_lst3[y] == "REDACTEDous") or (email_lst3[y] == "REDACTEDly") or (email_lst3[y] == "REDACTED\'s") or (email_lst3[y] == "REDACTED.") or (email_lst3[y] == "REDACTEDs") or (email_lst3[y] == "REDACTED!") or (email_lst3[y] == "REDACTEDe."):
+            if y == 0 and (not email_lst3[1] == "lineBreak"):
+                email_lst3[y + 1] = "REDACTED"
+                continue
+            if not y == (email_lst3.index(email_lst3[-1])):
+                if not email_lst3[y + 1] == "lineBreak":
+                    email_lst3[y + 1] = "REDACTED"
+                if not email_lst3[y - 1] == "lineBreak":
+                    email_lst3[y - 1] = "REDACTED"
+            y += 2
+        y += 1
+    email_lst1[0] = " ".join(email_lst3)
+    email_lst1[0] = email_lst1[0].replace("lineBreak", "\n")
+    return email_lst1[0]
+
+print(censor_email_four(email_four))
